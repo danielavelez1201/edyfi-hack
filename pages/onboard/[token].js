230328675ec4db7890b212e5a6289c70ca8669e8
SilenNaihin <silen.naihin@gmail.com>
2021-10-24 18:31:12 +0900

    adding projects

--- pages/onboard/[token].js
+++ pages/onboard/[token].js
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router'
 import axios from 'axios'
 import { Formik, Form } from 'formik'
+import { useState, useEffect } from 'react'
 import * as Yup from 'yup'
 import 'yup-phone'
 import { TextInput } from '../../components/TextInput'
@@ -9,63 +10,133 @@ export default function Onboarding() {
   const router = useRouter()
   const communityId = router.asPath.split('/')[2]
 
+  const [addProject, setAddProject] = useState(true)
+  const [projects, setProjects] = useState([])
+  const [newProject, setNewProject] = useState('')
+  const [buttonElement, setButtonElement] = useState('')
+
+  function projectEnter(event) {
+    if (event.key === 'Enter') {
+      projectAdd()
+    }
+  }
+
+  function projectAdd() {
+    if (projects.includes(newProject)) {
+      setButtonElement('Choice Already Exists!')
+    } else if (newProject === '') {
+      setButtonElement('Enter Something')
+    } else {
+      setProjects([...projects, newProject])
+      setButtonElement('')
+    }
+  }
+
+  function addCard() {
+    setAddProject(false)
+  }
+
+  function removeProject(project) {
+    setProjects(projects.filter((c) => c !== project))
+  }
+
+  const requiredError = 'Required'
+  const charError = 'Must be 40 characters or less'
+
   return (
-    <div className="h-screen flex bg-gradient-to-r from-indigo-dark via-gray to-indigo-light">
-    <div class='w-full max-w-md m-auto bg-white rounded-lg drop-shadow py-10 px-16'>
-      <Formik
-        initialValues={{
-          firstName: '',
-          lastName: '',
-          email: '',
-          phone: '',
-          location: '',
-          workingAt: '',
-          referral: 'yes', // TODO: change to select or checkbox
-          communityId: communityId
-        }}
-        validationSchema={Yup.object({
-          firstName: Yup.string().max(40, '^ Must be 40 characters or less').required('^ Required'),
-          lastName: Yup.string().max(40, '^ Must be 40 characters or less').required('^ Required'),
-          email: Yup.string().email('^ Invalid email address').required('^ Required'),
-          phone: Yup.string().phone('^ Enter a valid phone including +country code').required('^ Required'),
-          location: Yup.string().max(40, '^ Must be 40 characters or less').required('^ Required'),
-          workingAt: Yup.string().max(40, '^ Must be 40 characters or less').required('^ Required'),
-          referral: Yup.string().oneOf(['yes', 'no'])
-        })}
-        onSubmit={async (values, { setSubmitting }) => {
-          setSubmitting(true)
-          await axios.post('/api/signup', {
-            ...values
-          })
-          setSubmitting(false)
-        }}
-      >
-        {(formikProps) => (
+    <div className='h-screen flex bg-gradient-to-r from-indigo-dark via-gray to-indigo-light'>
+      <div class='w-full max-w-md m-auto bg-white rounded-lg drop-shadow py-10 px-16'>
+        <Formik
+          initialValues={{
+            firstName: '',
+            lastName: '',
+            email: '',
+            phone: '',
+            location: '',
+            work: '',
+            role: '',
+            refer: 'yes', // TODO: change to select or checkbox
+            communityId: communityId
+          }}
+          validationSchema={Yup.object({
+            firstName: Yup.string().max(40, charError).required(requiredError),
+            lastName: Yup.string().max(40, charError).required(requiredError),
+            email: Yup.string().email('Invalid email address').required(requiredError),
+            phone: Yup.string().phone('Enter a valid phone including +country code').required(requiredError),
+            location: Yup.string().max(40, charError).required(requiredError),
+            work: Yup.string().max(40, charError).required(requiredError),
+            role: Yup.string().max(40, charError).required(requiredError),
+            projectName: Yup.string().max(40, charError).required(requiredError),
+            projectLink: Yup.string().max(40, charError).required(requiredError),
+            refer: Yup.string().oneOf(['yes', 'no'])
+          })}
+          onSubmit={async (values, { setSubmitting }) => {
+            setSubmitting(true)
+            await axios.post('/api/signup', {
+              ...values
+            })
+            setSubmitting(false)
+          }}
+        >
+          {(formikProps) => (
+            <Form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+              <h1 class='text-2xl font-medium text-primary mt-4 mb-12 text-center'>
+                🏡 Join as a member of community {communityId}.
+              </h1>
 
-          <Form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-              <h1 class="text-2xl font-medium text-primary mt-4 mb-12 text-center">🏡 Join as a member of community {communityId}.</h1>
-
-            <div style={{ margin: '0 20px', textAlign: 'center' }}></div>
-            <TextInput label='First Name' name='firstName' type='text' placeholder='First name' class='w-full p-2 bg-gray-light text-primary  rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4' />
-            <TextInput label='Last Name' name='lastName' type='text' placeholder='Last name' class='w-full p-2 bg-gray-light text-primary  rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4' />
-            <TextInput label='Email Address' name='email' type='email' placeholder='Email' class='w-full p-2 bg-gray-light text-primary  rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4' />
-            <TextInput label='Phone' name='phone' type='text' placeholder='Phone' class='w-full p-2 bg-gray-light text-primary  rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4' />
-            <TextInput label='Location' name='location' type='text' placeholder='Location' class='w-full p-2 bg-gray-light text-primary  rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4' />
-            <TextInput label='Working at' name='workingAt' type='text' placeholder='Working at' class='w-full p-2 bg-gray-light text-primary  rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4' />
-            <TextInput label='referral' name='referral' type='text' placeholder='Referral' class='w-full p-2 bg-gray-light text-primary  rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4' />
-            <div style={{ margin: '0 20px 20px 20px', textAlign: 'center' }}>
-              <button
-                class='bg-blue py-2 px-4 text-sm text-white rounded  focus:outline-none focus:border-green-dark hover:bg-blue-hover '
-                type='submit'
-                disabled={formikProps.isSubmitting || !formikProps.isValid}
-              >
-                {formikProps.isSubmitting ? 'loading...' : 'Join community'}
-              </button>
-            </div>
-          </Form>
-        )}
-      </Formik>
+              <div style={{ margin: '0 20px', textAlign: 'center' }}></div>
+              <TextInput label='First Name' name='firstName' type='text' placeholder='first name' />
+              <TextInput label='Last Name' name='lastName' type='text' placeholder='last name' />
+              <TextInput label='Email Address' name='email' type='email' placeholder='email' />
+              <TextInput label='Phone' name='phone' type='text' placeholder='phone' />
+              <TextInput label='Location' name='location' type='text' placeholder='location' />
+              <TextInput label='Work' name='work' type='text' placeholder='work' />
+              <TextInput label='Role' name='role' type='role' placeholder='role' />
+              <div className='mt-2'>
+                {projects.map((project) => (
+                  <div className='flex'>
+                    <TextInput label='Project Name' name='projectName' type='text' placeholder='name' />
+                    <TextInput label='Project Link' name='projectLink' type='text' placeholder='link' />
+                  </div>
+                ))}
+              </div>
+              <div className='my-1 ml-1 '>
+                {addProject ? (
+                  <button className='shadow px-2 py-1' onClick={addCard}>
+                    Add a project
+                  </button>
+                ) : (
+                  <>
+                    <div className='flex'>
+                      <input
+                        className='border-b border-gray w-4/5'
+                        onKeyPress={projectEnter}
+                        value={newProject}
+                        placeholder='Type here...'
+                        onChange={(e) => setNewProject(e.target.value)}
+                      />
+                      <button className='ml-3' onClick={projectAdd}>
+                        <div className='h-6 w-6'>✅</div>
+                      </button>
+                    </div>
+                    <div className='text-sm text-red-300'>{buttonElement}</div>
+                  </>
+                )}
+              </div>
+              <TextInput label='Refer' name='refer' type='text' placeholder='refer' />
+              <div style={{ margin: '0 20px 20px 20px', textAlign: 'center' }}>
+                <button
+                  class='bg-blue py-2 px-4 text-sm text-white rounded  focus:outline-none focus:border-green-dark hover:bg-blue-hover '
+                  type='submit'
+                  disabled={formikProps.isSubmitting || !formikProps.isValid}
+                >
+                  {formikProps.isSubmitting ? 'loading...' : 'Join community'}
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
-  </div>
   )
 }
