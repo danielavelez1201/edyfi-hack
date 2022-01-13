c7762207fe71c6a60e611755ceef8b2563ff62ed
SilenNaihin <silen.naihin@gmail.com>
2022-01-13 12:12:06 -0500

    working on autoresponder

--- pages/api/textBumps.js
+++ pages/api/textBumps.js
@@ -3,7 +3,7 @@ import db from '../../firebase/clientApp'
 
 const authToken = process.env.TWILIO_AUTH_TOKEN
 const accountSid = process.env.TWILIO_ACCOUNT_SID
-// const client = require('twilio')(accountSid, authToken);
+const Twilio = require('twilio')(accountSid, authToken)
 // cronJob = require('cron').CronJob;
 
 // var textJob = new cronJob( '0 18 * * *', function(){
@@ -12,25 +12,78 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID
 //   },  null, true);
 
 async function handler(req, res) {
-  // const communityId = req.headers.communityid;
-  // const q = query(collection(db, 'users'), where("communityId", "==", communityId));
   const q = query(collection(db, 'users'))
 
   const users = await getDocs(q)
-  let userCommunities = [] // user:[[community1,community2],phone]
+  let userCommunities = []
 
   users.forEach((user) => {
     let userData = user.data()
-    // console.log(userData)
+    console.log(userData)
     userCommunities.push(userData)
   })
   let today = new Date().getTime()
-  
-  userCommunities.forEach((user)=> {
-    // console.log('here',user)
-    if (user.lastUpdated <= today - 7889400000) {
-      // textJob.start();
-      console.log('HERE', user.firstName)
+
+  userCommunities.forEach((user) => {
+    if (user.lastUpdated <= today - 26914471572 && user.lastSent <= today - 2629800000) {
+      // 7889400000 - actual number
+      user.lastSent = today // IMPLEMENT IN FB
+      await Twilio.messages
+        .create({
+          body: `Hey, ${user.firstName}! It's been a few months since you updated your information for ${
+            user.communityId
+          }: 1) ${user.firstName} ${user.lastName}, 2) ${user.email}, in 3) ${location}, 4) ${user.role} at 5) ${
+            user.work
+          }, working on projects 6) ${projects.map((p) => `${p}, `)}, and you 7) ${offers.map(
+            (h) =>
+              `${
+                h == 'investors'
+                  ? `can intro to investors${offers.length > 1 && ', '}`
+                  : h == 'cofounders'
+                  ? `${offers.length > 1 && 'and '}are searching for cofounders${offers.length > 2 && ', '}`
+                  : h == 'refer'
+                  ? `${offers.length > 2 && 'and '}can refer to a job${offers.length > 3 && ', '}`
+                  : h == 'hiring'
+                  ? `${offers.length > 3 && 'and '}are hiring`
+                  : null
+              }`
+          )}. Wanna update any field (yes/no)?`,
+          from: '+15593541895',
+          to: `+${user.phone}`
+        })
+        .then((message) => console.log(message.sid))
+
+      let twiml = new Twilio.twiml.MessagingResponse()
+
+      const body = event.Body ? event.Body.toLowerCase() : null
+      switch (body) {
+        case 'yes':
+          twiml.message('To update send one text with the following format: 2,newemail@gmail.com,3,NYC')
+          user.lastUpdated = today // if they respond
+          user.updated = today
+          break
+        case 'no':
+          twiml.message(
+            `Great! Check out the community if you haven't in a while: https://keeploop.io/onboard/${user.communityId}`
+          )
+          user.lastUpdated = today // if they respond
+          user.updated = today
+          break
+        case ',':
+          twiml.message(
+            `Great! Check out the community if you haven't in a while: https://keeploop.io/onboard/${user.communityId}`
+          )
+          user.lastUpdated = today // if they respond
+          user.updated = today
+        default:
+          twiml.message('Sorry, I only understand yes, no, or a correct update response')
+          break
+      }
+
+      callback(null, twiml)
+
+      
+      console.log('HERE', user.lastUpdated)
     }
   })
   //   await client.messages
