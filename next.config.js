a94f971cd4b2ff61c9a46115fa9b9c8a912f6082
Daniela Velez <danielavelez1201@gmail.com>
2022-01-22 13:39:40 -0500

    Move google png to public folder

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 module.exports = {
-  resolve: {
-    fallback: {
-      crypto: require.resolve('crypto-browserify')
-    }
-  },
-  webpack5: false
-  // webpack: (config) => {
-  //   config.resolve.fallback = { fs: false, crypto: false }
-  //   //   config.experiments = { topLevelAwait: true }
-  //   //   config.resolve.alias.https = 'https-browserify'
-  //   //   config.resolve.alias.http = 'http-browserify'
-  //   config.resolve.mainFields = ['browser']
-  //   return config
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    return config
+  }
 }
