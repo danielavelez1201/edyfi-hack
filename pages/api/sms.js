b7c72fe74f2bdb8990e9a23d78c7c311bf3ee274
SilenNaihin <silen.naihin@gmail.com>
2022-03-05 17:30:36 -0500

    fix

--- pages/api/sms.js
+++ pages/api/sms.js
@@ -5,9 +5,8 @@ handler = function (context, event, callback) {
   // Create a new messaging response object
   const twiml = new Twilio.twiml.MessagingResponse()
   // Use any of the Node.js SDK methods, such as `message`, to compose a response
-  twiml.message(context)
-  twiml.message(context.parameters.body)
-  twiml.message()
+  console.log(context)
+  twiml.message('context')
   // Return the TwiML as the second argument to `callback`
   // This will render the response as XML in reply to the webhook request
   return callback(null, twiml)
